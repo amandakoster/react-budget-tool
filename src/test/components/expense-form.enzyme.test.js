d5807e60ae2bcf4c099c6ebbba104764800d7592
Amanda Koster <amandakoster@gmail.com>
2017-12-18 17:04:47 -0800

    testing

--- src/test/components/expense-form.enzyme.test.js
+++ src/test/components/expense-form.enzyme.test.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import ExpenseForm from './../components/ExpenseForm';
+import ExpenseForm from '../../components/ExpenseForm';
 
 describe('<ExpenseForm />', () => {
-
-  let name = 'foo';
-  let amount = 10;
-
-  let expenseCreate = (state) => {
-    expect(ExpenseForm.state().name).toEqual(name);
-    expect(ExpenseForm.state().amount).toEqual(amount);
-  };
-
-  let ExpenseForm = shallow(<ExpenseForm handler={expenseCreate} />);
-
-  ExpenseForm.find('.item').simulate( 'change', {target: {name:'name', value:'name'} });
-  ExpenseForm.find('item').simulate( 'change', {target: {name:'amount', value: 'amount'} });
-  ExpenseForm.find('form').simulate( 'submit', { preventDefault: () => {} });
-
-});
   
+  test('properly adds a new expense', () => {
+    
+    let name = 'Foo';
+    let amount = 10;
+    
+    let expenseCreate = (state) => {
+      expect(expenseForm.state().name).toEqual(name);
+      expect(expenseForm.state().amount).toEqual(amount);
+    };
+    
+    let expenseForm = shallow(<ExpenseForm handler={expenseCreate} />);
+    
+    expenseForm.find('.item').simulate( 'change', { target: {name:'name', value:name} } );
+    expenseForm.find('.amount').simulate( 'change', { target: {name:'amount', value:amount} } );
+    expenseForm.find('form').simulate( 'submit', { preventDefault:()=>{} } );
+    
+  });
+  
+});
