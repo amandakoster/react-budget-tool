65d25e9991bf3561fb2b2eaefee8bd5a271402ae
Amanda Koster <amandakoster@gmail.com>
2017-12-19 11:59:14 -0800

    testing broke

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,64 @@
-const path = require('path');
+'use strict';
 
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const htmlWebpackPluginConfig = new htmlWebpackPlugin({
-  template  : './client/index.html',
-  filename  : 'index.html',
-  inject    : 'body'
-});
+// Dynamic Script and Style Tags
+const HTMLPlugin = require('html-webpack-plugin');
+
+// Makes a separate CSS bundle
+const ExtractPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry   : './client/index.js',
-  output  : {
-    path  : path.resolve('dist'),
-    filename : 'index_bundle.js',
-    publicPath: '/'
-  },
-  module  : {
-    loaders : [
-      { 
-        test : /\.js$/, 
-        loader : 'babel-loader', 
-        exclude : /node_modules/
-      },
-      { 
-        test : /\.jsx$/, 
-        loader : 'babel-loader', 
-        exclude : /node_modules/
-      },
+    
+  // Load this and everythning it cares about
+  entry: `${__dirname}/src/main.js`,
+
+  devtool: 'source-map',
+
+  // Stick it into the "path" folder with that file name
+  output: {
+    filename: 'bundle.[hash].js',
+    path: `${__dirname}/build`,
+  }, 
+
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/src/index.html`,
+    }),
+    new ExtractPlugin('bundle.[hash].css'),
+  ],
+
+  module: {
+    rules: [
+      // If it's a .js file not in node_modules, use the babel-loader
       {
-        test : /\.scss$/,
-        loader : 'style-loader!css-loader!sass-loader'
-      }
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },  
+      // If it's a .scss file 
+      {
+        test: /\.scss$/,
+        loader: ExtractPlugin.extract({
+          // These get loaded in reverse order and the output of one pipes into the other (think of a then)
+          use: [
+            {
+              loader: 'css-loader', 
+              options: {
+                sourceMap:true,
+              },
+            },
+            'resolve-url-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                includePaths:[`${__dirname}/src/style`],
+              },
+            },
+          ],
+        }),
+      },
+      
     ],
   },
-  devServer : {
-    historyApiFallback: true,
-  },
-  plugins : [htmlWebpackPluginConfig],
-}
+    
+};
